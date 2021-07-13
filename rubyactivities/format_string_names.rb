65395b32521ce520ae8e6831d377982e7d84fa61
theIanMilan <theIanMilan@users.noreply.github.com>
2021-07-13 18:04:46 +0800

    Shortened code logic and corrected test cases

--- rubyactivities/format_string_names.rb
+++ rubyactivities/format_string_names.rb
@@ -9,23 +9,13 @@ def format_string_of_names(array)
     return ''
   end
 
-  list = array.reduce([]) { |acc, hash|
-    acc << hash[:name]
-  }
-
-  if array.length >= 2
-    list[-1] = "& #{list[-1]}"
-  end
-  
-  if array.length > 2
-    list.join(', ')
-  else
-    list.join(' ')
-  end
+  list = array.reduce([]) {|acc, hash| acc << hash[:name]}
+  last_name = list.pop
+  list.length <= 0 ? last_name : "#{list.join(', ')} & #{last_name}"
 end
 
-p format_string_of_names(list0) == "Bart, Lisa, Maggie, & Homer"
-p format_string_of_names(list1) == "Bart, Lisa, & Maggie"
+p format_string_of_names(list0) == "Bart, Lisa, Maggie & Homer"
+p format_string_of_names(list1) == "Bart, Lisa & Maggie"
 p format_string_of_names(list2) == "Bart & Lisa"
 p format_string_of_names(list3) == "Bart"
 p format_string_of_names(list4) == ""
