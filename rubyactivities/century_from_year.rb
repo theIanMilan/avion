f527552d2fb7089be15f88e22db0c9eb34e21e09
theIanMilan <theIanMilan@users.noreply.github.com>
2021-07-15 19:11:43 +0800

    Modify logic to work for years regardless of digit length

--- rubyactivities/century_from_year.rb
+++ rubyactivities/century_from_year.rb
@@ -1,12 +1,18 @@
 def centuryFromYear(year)
-  if year.to_s[2..3].to_i.between?(01,99)
+  return 1 if year < 100
+  
+  if year.to_s[-2..-1].to_i.between?(01,99)
     year += 100
   end
 
-  year.to_s[0..1].to_i
+  year.to_s[0..(year.to_s.length - 3)].to_i
 end
 
 p centuryFromYear(1705) == 18
 p centuryFromYear(1900) == 19
 p centuryFromYear(1601) == 17
 p centuryFromYear(2000) == 20
+p centuryFromYear(900) == 9
+p centuryFromYear(901) == 10
+p centuryFromYear(90) == 1
+p centuryFromYear(9) == 1
